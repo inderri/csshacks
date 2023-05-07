c60227df047d8886d96478384ce7b9e7edbf8e14
inderri <99542126+inderri@users.noreply.github.com>
2023-05-07 19:15:04 +0500

    Delete interwebz v1 source code directory

--- interwebz v1 source code/CESP.h
+++
@@ -1,8 +0,0 @@
-class CESP
-{
-public:
-	void DrawHeader( int x, int y, int w, int r, int g, int b, int HealthBarWidth );
-	void DrawRadar( void );
-	void DrawChickenESP( void );
-	void draw( void );
-};
